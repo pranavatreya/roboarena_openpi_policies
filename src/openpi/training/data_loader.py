8e2b5f9bfff03694b690883b423cbeca478cc240
pranavatreya <pranavatreya@utexas.edu>
2025-06-29 21:32:34 +0000

    adjustments to get hosting to work on tpus

--- src/openpi/training/data_loader.py
+++ src/openpi/training/data_loader.py
@@ -6,7 +6,6 @@ from typing import Protocol, SupportsIndex, TypeVar
 
 import jax
 import jax.numpy as jnp
-import lerobot.common.datasets.lerobot_dataset as lerobot_dataset
 import numpy as np
 import torch
 
@@ -89,20 +88,22 @@ def create_dataset(data_config: _config.DataConfig, model_config: _model.BaseMod
     if repo_id == "fake":
         return FakeDataset(model_config, num_samples=1024)
 
-    dataset_meta = lerobot_dataset.LeRobotDatasetMetadata(repo_id, local_files_only=data_config.local_files_only)
-    dataset = lerobot_dataset.LeRobotDataset(
-        data_config.repo_id,
-        delta_timestamps={
-            key: [t / dataset_meta.fps for t in range(model_config.action_horizon)]
-            for key in data_config.action_sequence_keys
-        },
-        local_files_only=data_config.local_files_only,
-    )
+    #dataset_meta = lerobot_dataset.LeRobotDatasetMetadata(repo_id, local_files_only=data_config.local_files_only)
+    #dataset = lerobot_dataset.LeRobotDataset(
+    #    data_config.repo_id,
+    #    delta_timestamps={
+    #        key: [t / dataset_meta.fps for t in range(model_config.action_horizon)]
+    #        for key in data_config.action_sequence_keys
+    #    },
+    #    local_files_only=data_config.local_files_only,
+    #)
 
-    if data_config.prompt_from_task:
-        dataset = TransformedDataset(dataset, [_transforms.PromptFromLeRobotTask(dataset_meta.tasks)])
+    #if data_config.prompt_from_task:
+    #    dataset = TransformedDataset(dataset, [_transforms.PromptFromLeRobotTask(dataset_meta.tasks)])
 
-    return dataset
+    #return dataset
+
+    return None # we want to remove lerobot imports, and this function will never be called anyway
 
 
 def transform_dataset(dataset: Dataset, data_config: _config.DataConfig, *, skip_norm_stats: bool = False) -> Dataset:
